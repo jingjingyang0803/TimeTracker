442d6985d579a84ef3d281474806aaa0d4cc32ea
jingjing yang <jingdingdu@gmail.com>
2023-11-01 02:07:44 +0200

    add feature to set observation interval

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,6 +1,72 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Summary = () => {
+  const [tasks, setTasks] = useState([]);
+  const [start, setStart] = useState(new Date().setHours(0, 0, 0, 0));
+  const [end, setEnd] = useState(Date.now());
+
+  // Fetch tasks from the server on component mount
+  useEffect(() => {
+    fetch("http://localhost:3010/tasks")
+      .then((response) => response.json())
+      .then((data) => {
+        setTasks(data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setEnd(Date.now());
+    }, 1000); // update every second
+
+    return () => clearInterval(interval); // clear interval on component unmount
+  }, []);
+
+  const tasksOfInterest = tasks.filter((task) =>
+    task.startTime.some((time) => time >= start && time <= end)
+  );
+
+  const handleStartChange = (event) => {
+    setStart(new Date(event.target.value).getTime());
+  };
+
+  const handleEndChange = (event) => {
+    setEnd(new Date(event.target.value).getTime());
+  };
+
+  // Calculate the total active time of each task by summing up the durations of all activity periods within the observation interval
+  const formatTime = (task) => {
+    // The function calculates the total active time of each task within a specified observation interval (from `start` to `end`).
+    let timeInMs = task.startTime.reduce((total, startTime, i) => {
+      // `startTime` and `i` are the elements and indices of the `task.startTime` array respectively, while `total` is the accumulator that collects the sum of all durations.
+      if (
+        (startTime >= start && startTime <= end) ||
+        (task.stopTime[i] >= start && task.stopTime[i] <= end)
+      ) {
+        // The `if` condition checks whether the start time or stop time of any activity period falls within the observation interval.
+        let duration =
+          task.isActive && i === task.startTime.length - 1
+            ? // If the task is currently active (i.e., `task.isActive` is `true`) and the current start time is the last one in the `task.startTime` array (`i === task.startTime.length - 1`), the duration is calculated as the difference between the current time (`Date.now()`) and the largest of the start time and the start of the observation interval (`Math.max(startTime, start)`).
+              Date.now() - Math.max(startTime, start)
+            : // For all other activity periods, the duration is calculated as the difference between the smallest of the stop time and the end of the observation interval (`Math.min(task.stopTime[i], end)`) and the largest of the start time and the start of the observation interval (`Math.max(startTime, start)`).
+              Math.min(task.stopTime[i], end) - Math.max(startTime, start);
+        return total + duration;
+      }
+      return total;
+    }, 0);
+
+    // These durations are then summed up to give the total active time of the task within the observation interval.
+    let seconds = Math.floor(timeInMs / 1000);
+    let minutes = Math.floor(seconds / 60);
+    let hours = Math.floor(minutes / 60);
+
+    seconds = seconds % 60;
+    minutes = minutes % 60;
+
+    return `${hours}h ${minutes}m ${seconds}s`;
+  };
+
   return (
     <div>
       <ul>
@@ -14,6 +80,26 @@ const Summary = () => {
           charts displaying daily activity times for selected tasks.
         </li>
       </ul>
+      <hr />
+      <br />
+      <h4>
+        Observation interval: {new Date(start).toLocaleString()} to{" "}
+        {new Date(end).toLocaleString()}
+      </h4>
+      <label>
+        Start Time:
+        <input type="datetime-local" onChange={handleStartChange} />
+      </label>
+      <label>
+        End Time:
+        <input type="datetime-local" onChange={handleEndChange} />
+      </label>
+      {tasksOfInterest.map((task) => (
+        <div key={task.id}>
+          <h2>{task.name}</h2>
+          <p>Active Time: {formatTime(task)}</p>
+        </div>
+      ))}
     </div>
   );
 };
