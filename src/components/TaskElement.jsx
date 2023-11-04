29f4e0792a54c0b12fa0a4c9ffe226795cf81b18
jingjing yang <jingdingdu@gmail.com>
2023-11-04 12:05:16 +0200

    add custom tag via input not prompt

--- src/components/TaskElement.jsx
+++ src/components/TaskElement.jsx
@@ -21,12 +21,20 @@ const TaskElement = ({
   const [editingTask, setEditingTask] = useState(false);
   const [newTaskName, setNewTaskName] = useState(name); // Initialize newTaskName with the existing task name
   const [nameError, setNameError] = useState(""); // State to store the name error message
+
   const [tagError, setTagError] = useState("");
+
+  const [newTag, setNewTag] = useState("");
+
+  const [customTagInput, setCustomTagInput] = useState(false);
+  const [customTag, setCustomTag] = useState(""); // Separate state for custom tag input
+  const [addTagError, setAddTagError] = useState(""); // State to store the name error message
 
   // Function to start editing the task name
   const startEditingTaskName = () => {
     setEditingTask(true);
 
+    setCustomTagInput(false); // Hide the custom tag input
     setTagError(""); // Clear the error message
   };
 
@@ -55,11 +63,17 @@ const TaskElement = ({
     handleRemoveTask(taskId); // Calls the handleRemoveTask function with taskId as argument
   };
 
-  // ================================= Add/Remove Tag ============================================================
-  // Function to add a new tag
+  // ================================= Add Tag ===================================================================
+  // Function to cancel editing the tag name
+  const cancelEditingTagName = () => {
+    setCustomTagInput(false);
+    setAddTagError(""); // Clear the error message
+  };
+
+  //Function to add a new tag
   const addTag = () => {
     const existingTags = [];
-    // For each task
+    //For each task
     tasks.forEach((task) => {
       // For each tag in the task
       task.tags.forEach((tag) => {
@@ -74,56 +88,79 @@ const TaskElement = ({
     const selectRef = useRef(null); // Reference to store the select element
 
     const handleTagSelection = (selectedTag) => {
-      // If selected tag is not 'Custom'
       if (selectedTag !== "Custom") {
         // Pass the selectedTag value to the handleAddTag function
         handleAddTag(taskId, selectedTag);
+        setCustomTagInput(false); // Hide the custom tag input
       } else {
-        // Prompt the user to enter a new tag
-        const newTag = prompt("Enter a new tag:");
-
-        if (newTag !== null) {
-          const trimmedTag = newTag.trim();
-          // If trimmed tag is not empty
-          if (trimmedTag !== "") {
-            // Pass the trimmedTag value to the handleAddTag function
-            handleAddTag(taskId, trimmedTag);
-          } else {
-            // Alert if tag is empty
-            alert("Tag cannot be empty. Please enter a valid tag.");
-          }
-        } else {
-          console.log("User clicked Cancel");
-        }
+        // If "Custom" is selected, show the custom tag input
+        setCustomTagInput(true);
       }
 
       // Reset the select list to an empty value
       selectRef.current.value = "";
 
+      setEditingTask(false); // Hide the edit task name input
       setTagError(""); // Clear the error message
+      setAddTagError(""); // Clear the error message
+    };
+
+    const saveCustomTag = () => {
+      if (customTag.trim() === "") {
+        setAddTagError(
+          "Tag name cannot be empty. Please enter a valid tag name."
+        );
+      } else if (tags.includes(customTag)) {
+        setAddTagError("This task already has this tag.");
+      } else {
+        handleAddTag(taskId, customTag);
+        setCustomTagInput(false); // Hide the custom tag input
+        setCustomTag(""); // Clear the custom tag input field
+        setAddTagError(""); // Clear the error message
+      }
     };
 
     // Return select element with existing tags and option to add custom tag
     return (
-      <select
-        defaultValue=""
-        onChange={(e) => handleTagSelection(e.target.value)}
-        ref={selectRef}
-      >
-        <option value="" disabled>
-          Add a new tag...
-        </option>
-        {existingTags.map((tag, index) => (
-          // Disable tag if it's already included
-          <option key={index} value={tag} disabled={tags.includes(tag)}>
-            {tag}
+      <div>
+        <select
+          defaultValue=""
+          onChange={(e) => handleTagSelection(e.target.value)}
+          ref={selectRef}
+        >
+          <option value="" disabled>
+            Add a new tag...
           </option>
-        ))}
-        <option value="Custom">Custom</option>
-      </select>
+          {existingTags.map((tag, index) => (
+            <option key={index} value={tag} disabled={tags.includes(tag)}>
+              {tag}
+            </option>
+          ))}
+          <option value="Custom">Custom</option>
+        </select>
+
+        {customTagInput && (
+          <div>
+            <input
+              type="text"
+              value={customTag}
+              onChange={(e) => {
+                setCustomTag(e.target.value);
+                setAddTagError(""); // Clear the error message when the user starts typing
+              }}
+              autoFocus
+            />
+            <button onClick={saveCustomTag}>Save</button>
+            <button onClick={cancelEditingTagName}>Cancel</button>
+          </div>
+        )}
+
+        {addTagError && <div className="error-message">{addTagError}</div>}
+      </div>
     );
   };
 
+  // ================================= Remove Tag ================================================================
   const removeTag = (index) => {
     if (tags.length === 1) {
       setTagError(
@@ -170,6 +207,7 @@ const TaskElement = ({
               setNewTaskName(e.target.value);
               setNameError(""); // Clear the error message when the user starts typing
             }}
+            autoFocus
           />
           <button onClick={saveTaskName}>Save</button>
           <button onClick={cancelEditingTaskName}>Cancel</button>
@@ -218,9 +256,9 @@ const TaskElement = ({
             </button>
           </span>
         ))}
+        {tagError && <div className="error-message">{tagError}</div>}
         {addTag()}{" "}
         {/* Render the addTag function to display the select input for adding a new tag */}
-        {tagError && <div className="error-message">{tagError}</div>}
       </div>
     </div>
   );
